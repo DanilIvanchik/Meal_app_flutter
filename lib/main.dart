3c2ee690131d4a8298e76c6cc0d04e0fc5cfec7f
Daniil Ivanchyk <ivanchikdanil@gmail.com>
2023-11-06 20:41:21 +0200

    added font family

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,80 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBar(),
+      body: Column(children: [
+        Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0)
+          ]),
+          child: TextField(
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Icon(Icons.search),
+                suffixIcon:
+                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  VerticalDivider(
+                    color: Colors.grey.shade200,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.menu_book),
+                  )
+                ]),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none)),
+          ),
+        )
+      ]),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Breakfast",
+        style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Poppins'),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      leading: GestureDetector(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Icon(Icons.menu),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          child: Container(
+            height: 37,
+            width: 37,
+            margin: EdgeInsets.all(10),
+            child: Icon(Icons.more_horiz),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
