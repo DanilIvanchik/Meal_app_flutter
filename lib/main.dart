56f1ce04c57582d6fd5e23c576763b2fba429b10
Daniil Ivanchyk <ivanchikdanil@gmail.com>
2023-11-06 22:29:19 +0200

    Added full home page

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
